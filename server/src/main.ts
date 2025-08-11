bc0160e1d892491f18733ac1bba707f932d18cbe
Boris Ushakov <inlvert@gmail.com>
2025-08-11 13:47:01 +0300

    add change to main.ts file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CONSTANTS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(CONSTANTS.PORT ?? 3000);
 }
 bootstrap();
