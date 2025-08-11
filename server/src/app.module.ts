bb80cfa3ca5883ea3c178e38cd4ed5166712eea1
Boris Ushakov <inlvert@gmail.com>
2025-08-11 13:46:11 +0300

    add change app.midule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,51 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './users/model/user.model';
+import { UsersModule } from './users/users.module';
+import { CONSTANTS } from './constants';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        dialect: 'postgres',
+        uri: CONSTANTS.DB_CONNECT,
+        autoLoadModels: true,
+        synchronize: true,
+        logging: false,
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
+//     SequelizeModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: (configService: ConfigService) => ({
+//         dialect: 'postgres',
+//         uri: configService.get<string>('DB_CONNECT'),
+//         // models: [User],
+//         autoLoadModels: true,
+//         synchronize: true,
+//         logging: false,
+//         // dialectOptions: {
+//         //   ssl: {
+//         //     require: true,
+//         //     rejectUnauthorized: false,
+//         //   },
+//         // },
+//       }),
+//     }),
+//     UsersModule,
+//   ],
+// })
+// export class AppModule {}
