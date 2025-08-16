68abe80d841f33901141b4c4dc923f08a2d8e96a
Boris Ushakov <inlvert@gmail.com>
2025-08-16 23:41:31 +0300

    add changes  in user service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,16 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './model/user.model';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User) private userModel: typeof User) {}
 
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const existUser = await this.userModel.findOne({
+      where: { email: createUserDto.email },
+    });
+    if (existUser) {
+      throw new Error('user alredy exist');
+    }
+
+    const hashedPassword = await argon2.hash(createUserDto.password);
+
+    const newUser = await this.userModel.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
+    return newUser;
+  }
+
   async getAllUsers(): Promise<User[]> {
     return this.userModel.findAll();
   }
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    return this.userModel.create(createUserDto);
+  async findOne(email: string): Promise<User | undefined> {
+    const user = await this.userModel.findOne({ where: { email } });
+
+    return user ?? undefined;
   }
 }
