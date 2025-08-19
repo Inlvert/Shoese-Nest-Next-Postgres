3618dbf291e86be042bc9fce8e9c33269dfbcd37
Boris Ushakov <inlvert@gmail.com>
2025-08-19 10:08:51 +0300

    add change to constants

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,9 +2,10 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_CONNECT, PORT } = process.env;
+const { DB_CONNECT, PORT, JWT_SECRET } = process.env;
 
 export const CONSTANTS = {
   DB_CONNECT,
   PORT,
+  JWT_SECRET 
 };
