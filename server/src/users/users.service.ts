25c45c338db8a7f350f397a7cc078655164c7f65
Boris Ushakov <inlvert@gmail.com>
2025-08-20 10:31:21 +0300

    add JwtService and token in user.service

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -3,12 +3,17 @@ import { InjectModel } from '@nestjs/sequelize';
 import { User } from './model/user.model';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ResponseUserDto } from './dto/response-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userModel: typeof User) {}
+  constructor(
+    @InjectModel(User) private userModel: typeof User,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
+  async createUser(createUserDto: CreateUserDto): Promise<ResponseUserDto> {
     const existUser = await this.userModel.findOne({
       where: { email: createUserDto.email },
     });
@@ -18,12 +23,14 @@ export class UsersService {
 
     const hashedPassword = await argon2.hash(createUserDto.password);
 
-    const newUser = await this.userModel.create({
+    const user = await this.userModel.create({
       ...createUserDto,
       password: hashedPassword,
     });
 
-    return newUser;
+    const token = this.jwtService.sign({ id: user.id, email: user.email });
+
+    return { user, token };
   }
 
   async getAllUsers(): Promise<User[]> {
