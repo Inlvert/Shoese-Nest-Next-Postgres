12180d963e590f30b5475bb28869526cd6b02bcd
Boris Ushakov <inlvert@gmail.com>
2025-08-21 16:08:15 +0300

    add new methods to category controller

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -9,7 +21,35 @@ export class CategoryController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
   createCategory(@Body() createCategoryDto: CreateCategoryDto, @Req() req) {
-    return this.categoryService.create(createCategoryDto, req.user.id)
+    return this.categoryService.create(createCategoryDto, req.user.id);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  getAllCategories(@Req() req) {
+    return this.categoryService.getAll(req.user.id);
+  }
+
+  @Get(':categoryId')
+  @UseGuards(JwtAuthGuard)
+  getOneCategory(@Param('categoryId') categoryId: number) {
+    return this.categoryService.getOne(categoryId);
+  }
+
+  @Put(':categoryId')
+  @UseGuards(JwtAuthGuard)
+  getOneCategoryAndUpdate(
+    @Param('categoryId') categoryId: number,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    return this.categoryService.getOneAndUpdate(categoryId, createCategoryDto);
+  }
+
+  @Delete(':categoryId')
+  @UseGuards(JwtAuthGuard)
+  getOneCategoryAndDelete(@Param('categoryId') categoryId: number) {
+    return this.categoryService.getOneAndDelete(categoryId);
   }
 }
